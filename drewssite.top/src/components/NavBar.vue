8b0dd1c397722318758d337709dbdba5cb2e3ee1
Foxler2010 <drew@drewssite.top>
2022-11-28 19:45:34 +0000

    Clean up shop

---
+++ drewssite.top/src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  padding-top: 10px;
+  padding-bottom: 10px;
+  background-color: darkblue;
+}
+
+RouterLink {
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+RouterLink:hover {
+  background-color: darkviolet;
+  transition: background-color 2s;
+}
+</style>
