ce2b0f1dd031a33b9f8096928ed5ac04b242eff9
foxler2010 <drew@drewssite.top>
2023-01-20 02:31:26 +0000

    the header is getting somewhere

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,25 +2,42 @@
 
 <template>
   <main>
-    <div class="splash">
-      <img src="/splash.img" />
+    <div class="header">
+      <img src="../assets/splash.jpg" />
       <div class="box">
         <h1>Welcome to drewssite.top</h1>
       </div>
     </div>
+    <h1 v-for="n in 100" :key="n">{{ n }}</h1>
   </main>
 </template>
 
 <style scoped>
-
-.splash {
+.header {
   background-color: red;
 }
 
-.splash img {
-  background-color: green;
+.header img {
+  position: absolute;
+  width: 100%;
 }
 
 .box {
+  background-image: linear-gradient(to bottom right,purple, blue, purple);
+  padding: 2em;
+  border-radius: 1em;
+  position: absolute;
+  top: 25%;
+  bottom: 25%;
+  left: 10%;
+  right: 10%;
 }
+.box:hover {
+
+}
+.box h1 {
+  text-align: center;
+  font-family: sans-serif;
+}
+
 </style>
