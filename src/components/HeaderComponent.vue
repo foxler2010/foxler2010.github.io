d3c11c845e28f473242c88926eb1ecc71416bc7e
foxler2010 <drew@drewssite.top>
2023-04-24 19:55:08 +0000

    use props

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,10 +1,26 @@
-<script setup></script>
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  subtext: {
+    type: String,
+    required: false,
+    default: "",
+  },
+  buttonText: {
+    type: String,
+    required: true,
+  },
+});
+</script>
 
 <template>
   <div class="header">
-    <h1>Welcome to drewssite.top</h1>
-    <p>This is my website :)</p>
-    <RouterLink to="/about">Learn More!</RouterLink>
+    <h1>{{ title }}</h1>
+    <p v-if="subtext.length > 0">{{ subtext }}</p>
+    <RouterLink to="/about">{{ buttonText }}</RouterLink>
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,14 @@
 <script setup>
-import HeaderComponent from "../components/HomeView/HeaderComponent.vue";
+import HeaderComponent from "../components/HeaderComponent.vue";
 </script>
 
 <template>
   <main>
-    <HeaderComponent />
+    <HeaderComponent
+      title="Welcome to drewssite.top"
+      subtext="This is my website :)"
+      buttonText="Learn More!"
+    />
     <h1 v-for="n in 100" :key="n">{{ n }}</h1>
   </main>
 </template>
